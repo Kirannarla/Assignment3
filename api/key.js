da0fadad5668a0b18edc966548f02f4d796898de
Kirannarla <152807211+Kirannarla@users.noreply.github.com>
2024-04-19 10:26:08 -0500

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://kiranreddynarla:kirankumarreddy@cluster0.o367cvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
